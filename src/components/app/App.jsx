73409b5b81cac274f2367aba57e37ec1f10cfe9b
JorG1990 <jorge  garcia>
2023-08-21 18:34:26 -0500

    add app.jsx and app.styled.jsx

---
+++ src/components/app/App.jsx
@@ -0,0 +1,95 @@
+
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Searchbar from "";
+import Modal from "";
+import Button from "";
+import ImageGallery from "";
+import ImageGalleryItem from "";
+import MyLoader from "";
+import { GalleryContainer,SearchbarContainer, ButtonContainer } from "./App.styled";
+
+const keyApi = "38957997-b8acc7a81f773178d46be80fd";
+
+function App() {
+  const [images, setImages] = useState([]);
+  const [searchQuery, setSearchQuery] = useState('');
+  const [page, setPage] = useState(1);
+  const [isLoading, setIsLoading] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+  const [selectedImage, setSelectedImage] = useState(null);
+  const [showMoreButton, setShowMoreButton] = useState(false);
+
+  useEffect(() => {
+    const fetchImages = () => {
+      if (!searchQuery.trim()){
+        setImages([]);
+        setShowMoreButton(false);
+        return;
+      }
+
+    const url = `https://pixabay.com/api/?key=${keyApi}&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=12`;
+
+    setIsLoading(true);
+    axios
+      .get(url)
+      .then((response) => {
+        const { hits } = response.data;
+        setImages((prevImages) => [...prevImages, ...hits]);
+        setIsLoading(false);
+        setShowMoreButton(hits.length === 12);
+      })
+      .catch((error) => console.log(error));
+    };
+
+    fetchImages();
+  }, [searchQuery, page]);
+
+  const openModal =(imageUrl) => {
+    setShowModal(true);
+    setSelectedImage(imageUrl);
+  };
+
+  const closeModal = () => {
+    setShowModal(false);
+    setSelectedImage(null);
+  };
+
+  const handeleSearchbarSubmit = (query) => {
+    if (!query.trim()) {
+      setSearchQuery("");
+      return;
+    }
+
+    setSearchQuery(query);
+    setPage(1);
+    setImages([]);
+    setShowMoreButton(false);
+  };
+
+  return (
+    <>
+      <SearchbarContainer>
+        <Searchbar onSubmit={ handeleSearchbarSubmit } />
+      </SearchbarContainer>
+      <GalleryContainer>
+        { images.map((image, index) => (
+          <ImageGalleryItem key={ image.id + '_' + index } image={ image } onImageClick={ openModal } />
+        ))}
+        { isLoading ? (
+          <MyLoader />
+        ) : (
+          <ImageGallery images={ images } onImageClick={ openModal } />
+        )}
+        { showModal && <Modal imageUrl={ selectedImage } closeModal={ closeModal } />}
+      </GalleryContainer>
+      {showMoreButton && (
+        <ButtonContainer>
+          <Button onClick={() => setPage(page + 1)} disabled={ isLoading } showMoreButton={ true } />
+        </ButtonContainer>
+      )}
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/app/App.styled.jsx
@@ -0,0 +1,24 @@
+
+import styled from "styled-components";
+
+export const SearchbarContainer = styled.div`
+`;
+
+export const GalleryContainer = styled.div`
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    grid-gap: 10px;
+    justify-items: center;
+`;
+
+export const ContainerButtonCarga = styled.div`
+    display: flex;
+    justify-content: center;
+    margin-bottom: 20px;
+`;
+
+export const ButtonContainer = styled.div`
+    display: flex;
+    justify-content: center;
+    margin-bottom: 20px;
+`;
